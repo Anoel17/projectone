0e1f7e939afec658902f0092cc73df0b6e2864b9
edtaylor25 <e.davtaylor2@gmail.com>
2021-12-09 00:28:04 -0600

    Code for Login and Employee Home

--- src/app/home/home-employee/home-employee.component.ts
+++ src/app/home/home-employee/home-employee.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/users/user.service';
 
 @Component({
   selector: 'app-home-employee',
@@ -7,9 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeEmployeeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
-
+getUserInfo(){
+  this.router.navigate(['user-details']);
+}
 }
