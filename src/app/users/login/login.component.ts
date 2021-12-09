e0fd357a4a8a5fe73bfffb25cce1a5de483e054f
Reese <derricewright@gmail.com>
2021-12-09 15:03:49 -0500

    commit try for frontend

---
+++ src/app/users/login/login.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
+import { user } from '../user.model';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  email = "";
+  password = "";
+  errorMsg="";
+
+  testPerm = "";
+
+
+
+  currentUser: user = {
+    id:0,
+    screenName:"",
+    userType: "",
+    homeState: "",
+    homeTown: "",
+    address: "",
+    email: "",
+    password: ""
+  }
+  constructor(private userService: UserService, authService: AuthService, private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  performLogin() {
+    this.userService.getLoggedIn(this.email, this.password).subscribe(
+      
+      (response) => {
+        console.log(response);
+        sessionStorage.setItem('userId', ""+response.id);
+        sessionStorage.setItem('userLevel', response.userType);
+      },
+      (error) => {
+          console.log(error);
+          this.errorMsg= "ERROR!";
+          console.log(this.errorMsg);
+      }
+    );
+    
+
+  }
+
+}
