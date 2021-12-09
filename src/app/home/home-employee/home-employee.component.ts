e67a95c8c6068a401210282ab730c4b928bc1ed5
edtaylor25 <e.davtaylor2@gmail.com>
2021-12-09 02:25:09 -0600

    Added a button

--- src/app/home/home-employee/home-employee.component.ts
+++ src/app/home/home-employee/home-employee.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/users/user.service';
+import { RequestService } from 'src/app/request/request.service';
+import { RequestListPendingComponent } from 'src/app/request/request-list/request-list-pending/request-list-pending.component';
 
 @Component({
   selector: 'app-home-employee',
@@ -7,9 +11,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeEmployeeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
+getUserInfo(){
+  this.router.navigate(['user-details']);
+}
 
+getUserRequestService(){
+  this.router.navigate(['request-pending']);
+
+}
 }
